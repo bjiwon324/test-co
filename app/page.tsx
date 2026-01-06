bedcede14e528eddfcce63a0a06f21b2ad155f07
jiwon <bjiwon0324@gmail.com>
2026-01-06 14:11:41 +0900

    fix : add conect text and logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            ALL About
-          </h1>
-          <p className="text-lg break-keep leading-8 text-zinc-600 dark:text-zinc-400">
-            현재 페이지 리뉴얼 중입니다. 곧 멋진 모습으로 찾아뵙겠습니다!👍
+          <Image src={"/logo.jpg"} alt="로고" width={350} height={50} />
+          <p className="text-lg break-keep leading-8  text-zinc-800 dark:text-zinc-200">
+            현재 페이지 리뉴얼 중입니다. 곧 멋진 모습으로 찾아뵙겠습니다! 😉
             <br />
             감사합니다.
           </p>
-          <p className="text-lg break-keep leading-8 text-zinc-600 dark:text-zinc-400">
+          <p className="text-lg break-keep leading-8  text-zinc-800 dark:text-zinc-200">
             This page is currently under renovation. <br />
-            We will be back soon with a great new look!👍
+            We will be back soon with a great new look! 😉
             <br />
             Thank you.
+          </p>
+          <p className="pt-4 text-mg break-keep leading-8 font-bold text-zinc-800 dark:text-zinc-200">
+            문의 사항은 032-523-7177 로 연락 부탁드립니다.
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row"></div>
